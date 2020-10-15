61e0525c0c3ecc80a17c9b106f2083169986af80
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2020-10-15 20:46:05 +0400

    Update yan_tts_example.py

--- yan_tts_example.py
+++ yan_tts_example.py
@@ -1,5 +1,7 @@
 #############yan_tts_example.py#############
 from yan_tts import speech_to_text
 
-speech_to_text(input_wav_file = "/yan/test1.wav")
+text = speech_to_text(input_wav_file = "test3.wav")
+
+print(text)
 #############yan_tts_example.py#############
